c7cbc38f7609b118a3b7bf9e41287bcc4019a978
Neander de Souza <neander.faria@hotmail.com>
2023-07-14 10:46:19 -0300

    db: fixed pets in db

---
+++ src/repositories/interfaces/pets-repository.ts
@@ -0,0 +1,7 @@
+import { Pet, Prisma } from '@prisma/client'
+
+export interface PetsRepository {
+  create(data: Prisma.PetUncheckedCreateInput): Promise<Pet>
+  findById(id: string): Promise<Pet | null>
+  findPets(city: string, breed?: string): Promise<Pet[]>
+}
